a173d4802cfb2995a3d1915de17248dfc1aaf7e9
Alex Szabo <alex.szabo@prezi.com>
2021-10-07 17:22:52 +0200

    Step 2 - Introduce external models
    # Conflicts:
    #	README.md
    #	src/constants.ts

--- src/flightsScene/FlightsScene.tsx
+++ src/flightsScene/FlightsScene.tsx
@@ -1,35 +1,31 @@
 import {OrbitControls} from "@react-three/drei";
-import {Box} from "../Utilities";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useRef} from "react";
 import {PointLight} from "three";
 import {useFrame} from "@react-three/fiber";
+import Globe from "../models/Globe";
+import Plane from "../models/Plane";
+import {MAP_TOP, PLANE_SCALE} from "../constants";
 
 export default function FlightsScene() {
-  const [intensity, setIntensity] = useState(1.0);
   const lightRef = useRef<PointLight>();
-
-  useEffect(() => {
-    setInterval(() => {
-      setIntensity(Math.random() * 3.0);
-    }, 1000);
-  }, []);
 
   useFrame((state, delta) => {
     const phase = (state.clock.elapsedTime % 3) / 3;
     const phaseRadians = Math.PI * 2 * phase;
 
     if (lightRef.current) {
-      const x = Math.sin(phaseRadians) * 2;
-      const y = Math.cos(phaseRadians) * 2;
-      lightRef.current.position.set(x, y, x*y);
+      const x = Math.sin(phaseRadians) * 10;
+      const z = Math.cos(phaseRadians) * 10;
+      lightRef.current.position.set(x, 0, z);
     }
   });
 
   return (
     <>
       <OrbitControls/>
-      <pointLight ref={lightRef} intensity={intensity} position={[2, 2, 2]}/>
-      <Box position={[0, 0, 0]} color={'yellow'}/>
+      <pointLight ref={lightRef} intensity={5.0} position={[2, 2, 2]}/>
+      <Globe/>
+      <Plane scale={PLANE_SCALE} />
     </>
   )
 }
