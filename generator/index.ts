c800b3f72b2e900bc9e7cdeb578c70092c1cbbf9
Alex Szabo <alex.szabo@prezi.com>
2021-10-12 14:18:50 +0200

    Generator to create data folder

--- generator/index.ts
+++ generator/index.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env ts-node
 import { generateFlights, generateAirports } from './generator';
+import { mkdirSync } from 'fs';
 
 console.log('generating flight data...');
 
 let [daysSimulated, outputFolder] = process.argv.slice(2);
 
 outputFolder = outputFolder || process.cwd();
+
+mkdirSync(outputFolder);
 
 generateFlights(parseInt(daysSimulated) || 10, outputFolder);
 generateAirports(outputFolder);
